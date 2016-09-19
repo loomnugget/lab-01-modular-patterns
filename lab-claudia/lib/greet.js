486fea37ef44a08cc3ffdc8a95e79b88b234af78
Claudia <claudia.cedfeldt@gmail.com>
2016-09-19 14:15:20 -0700

    Changed gulp file syntax and added string interpolation instead of concatenation

--- lab-claudia/lib/greet.js
+++ lab-claudia/lib/greet.js
@@ -4,5 +4,5 @@
 module.exports = function greet(name) {
   if(!name) throw new Error('expected name'); //if nothing passed in, throws will be caught
   //Error is a constructor
-  return 'Hello ' + name; //concatenation
+  return `Hello ${name}`; //string interpolation
 };
